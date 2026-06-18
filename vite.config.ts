7dda409e47862146bcf7e549dc024fe4c330e3ef
prathamesh chavan <prathamesh011official@gmail.com>
2026-06-18 12:18:46 +0530

    react router vulnerability fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
 		reactRouter(),
-		tsconfigPaths(),
 	],
 	build: {
 		rollupOptions: {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    environment: "node",
-    include: ["app/**/*.test.ts"],
-  },
+	resolve: {
+		tsconfigPaths: true,
+	},
+	test: {
+		environment: "node",
+		include: ["app/**/*.test.ts"],
+	},
 });
